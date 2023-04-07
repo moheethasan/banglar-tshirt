2683fd2516a28a772b0002b438b2b8883cacd8e4
majesty-01 <moheethasan4@gmail.com>
2023-04-07 06:50:34 +0600

    added t-shirt card and handled duplicate product add

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLoaderData } from "react-router-dom";
+import Tshirt from "../Tshirt/Tshirt";
+import Cart from "../Cart/Cart";
+import "./Home.css";
+import toast from "react-hot-toast";
 
 const Home = () => {
   const tshirts = useLoaderData();
-  console.log(tshirts);
+
+  const [cart, setCart] = useState([]);
+  const handleAddToCart = (tshirt) => {
+    const exists = cart.find((ts) => ts._id === tshirt._id);
+    if (exists) {
+      toast("You have already added this t-shirt");
+      return;
+    }
+    const newCart = [...cart, tshirt];
+    setCart(newCart);
+  };
+
+  const handleRemoveFromCart = (id) => {
+    const remaining = cart.filter((ts) => ts._id !== id);
+    setCart(remaining);
+  };
+
   return (
-    <div>
-      <h2>home {tshirts.length}</h2>
+    <div className="home-container">
+      <div className="t-shirts-container">
+        {tshirts.map((tshirt) => (
+          <Tshirt
+            key={tshirt._id}
+            tshirt={tshirt}
+            handleAddToCart={handleAddToCart}
+          ></Tshirt>
+        ))}
+      </div>
+      <div className="cart-container">
+        <Cart cart={cart} handleRemoveFromCart={handleRemoveFromCart}></Cart>
+      </div>
     </div>
   );
 };
--- src/components/Layout/Main.jsx
+++ src/components/Layout/Main.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 import Header from "../Header/Header";
 import { Outlet } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 
 const Main = () => {
   return (
     <div>
       <Header></Header>
       <Outlet></Outlet>
+      <Toaster
+        toastOptions={{
+          className: "",
+          style: {
+            border: "1px solid black",
+            padding: "16px",
+            color: "white",
+            background: "crimson",
+          },
+        }}
+      />
     </div>
   );
 };
